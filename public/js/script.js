fb4e663ec73be54f925233d9f7b5245fd271b877
Portal-de-Cursos-Online-FC <portaldecursosonlinefc@gmail.com>
2022-12-11 19:53:28 +0100

    Subindo o projecto finar para o repositório remoto

---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+//global variables declaration
+const perguntaUsuario = document.querySelector('#inputPergunta');
+const resposta = document.querySelector('#resposta');
+const respostas = [
+    'Certeza!',
+    'Não tenho tanta certeza.',
+    'É decididamente assim.',
+    'Não conte com isso.',
+    'Sem dúvidas!',
+    'Pergunte novamente mais tarde.',
+    'Sim, definitivamente',
+    'Minha resposta é não.',
+    'Voce pode contar com isso.',
+    'Melhor não te dizer agora.',
+    'A meu ver, sim.',
+    'Minhas fontes dizem não.',
+    'Provavelmente',
+    'Não é possível prever agora',
+    'Perspectiva boa.',
+    'As espectativas não são tão boas.',
+    'Sim',
+    'Concentre-se novamente.',
+    'Sinais apontam que sim.'
+];
+
+//click oask question
+function fazerPergunta() {
+    if(perguntaUsuario.value == ''){
+        alert('Por favor, escreva sua pergunta!');
+        return;
+    }
+
+    const pergunta = '<div>' + perguntaUsuario.value + '<div>'
+
+//generate random number
+    const totalRespostas = respostas.length;
+    const respostaAleatoria = Math.floor(Math.random() * totalRespostas);
+    resposta.innerHTML = pergunta + respostas[respostaAleatoria];
+
+//hiding the question
+    setTimeout(function() {
+        resposta.style.opacity = 0;
+    }, 3000);
+}
